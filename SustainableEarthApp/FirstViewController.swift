4d38ad03549be4cf8bac2eacad57d7d5c9666033
Sophia Lau <slau2022@gmail.com>
2020-10-30 10:34:24 -0400

    Initial Commit

---
+++ SustainableEarthApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SustainableEarthApp
+//
+//  Created by Sophia Lau on 10/30/20.
+//  Copyright © 2020 Sophia Lau. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SustainableEarthApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SustainableEarthApp
+//
+//  Created by Sophia Lau on 10/30/20.
+//  Copyright © 2020 Sophia Lau. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
