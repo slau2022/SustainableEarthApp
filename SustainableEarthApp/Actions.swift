bd50e8f2d5753cdccac66bc2fcbbc1db06ad6787
Sophia Lau <slau2022@gmail.com>
2020-10-30 11:28:14 -0400

    Initial Commit

---
+++ SustainableEarthApp/Actions.swift
@@ -0,0 +1,30 @@
+//
+//  Actions.swift
+//  SustainableEarthApp
+//
+//  Created by Sophia Lau on 10/30/20.
+//  Copyright © 2020 Sophia Lau. All rights reserved.
+//
+
+import UIKit
+
+class Actions: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
