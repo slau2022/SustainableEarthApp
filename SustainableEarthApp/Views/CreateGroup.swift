ce6a2148dae20d7c4ae97fd1c7c0abb4c5b56e11
CasperWong-jpg <CasperWong-jpg@users.noreply.github.com>
2021-02-22 17:22:10 -0500

    Issue with adding database (crashes when creating new group)

--- SustainableEarthApp/Views/CreateGroup.swift
+++ SustainableEarthApp/Views/CreateGroup.swift
@@ -7,10 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateGroup: UITableViewController {
 
+    @IBOutlet weak var groupName: UITextField!
+    
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func groupCreated(_ sender: UIButton) {
+        if let name = groupName.text {
+            var ref: DocumentReference? = nil
+            ref = db.collection("communities").addDocument(data: [
+                "groupName": name
+            ]) { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document added with ID: \(ref!.documentID)")
+                }
+            }
+        }
     }
 }
