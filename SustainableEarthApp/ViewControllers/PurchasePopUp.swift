519406f23776a5004fa1887e8e31d6fdad4b02f2
Sophia Lau <slau2022@gmail.com>
2021-07-30 17:52:37 -0400

    Trying a new popup strategy

---
+++ SustainableEarthApp/ViewControllers/PurchasePopUp.swift
@@ -0,0 +1,30 @@
+//
+//  PurchasePopUp.swift
+//  SustainableEarthApp
+//
+//  Created by Sophia Lau on 7/30/21.
+//  Copyright © 2021 Sophia Lau. All rights reserved.
+//
+
+import UIKit
+
+class PurchasePopUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
